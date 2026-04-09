b0aa6ae0ccea911f6a9b12486cc085dac52adb61
Freddy Tomada <freddytomada7@gmail.com>
2026-04-09 11:18:26 -0400

    delete unused components

--- src/components/DraggableElement.tsx
+++
@@ -1,9 +0,0 @@
-import { useDraggable } from "@dnd-kit/react";
-
-export function Draggable({ children }: { children: React.ReactNode }) {
-  const { ref } = useDraggable({
-    id: "draggable",
-  });
-
-  return <button ref={ref}>{children}</button>;
-}
